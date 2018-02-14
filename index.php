acef78cbf28f218063ea3b2485ea169fb49cd96d
Raju Kumar <rakumar@csumb.edu>
2018-02-14 04:11:31 +0000

    1st

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Operating Sytems</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1> Operating Systems</h1>
+        </header>
+        <nav>
+            <hr width=50% />
+            <a href = "index.php">Home</a>
+            <a href = "linux.html">Linux</a>
+            <a href = "mac.html" >macOS</a>
+            <a href = "windows.html" >Windows</a>
+        </nav>
+        <main>
+        
+        <figure id="os">
+            <img src="img/3.png" alt="home picture" />
+        </figure>
+        <br>
+        
+        <p>
+            An operating system is a software that manages a computer's hardware and provides services for computer programs.
+            There are many types of Operating Systems available today. For the majority of the people,
+            there are 3 popular OS in used today. Windows, Linux, and macOS. Each has a different layout, and users may choose it
+            depending on what their preferences are.
+        </p>
+        </div>
+        
+        </main>
+        <footer>
+            <hr>
+            CST-336: Internet Programming. &copy2018; Kumar <br />
+            <strong>Disclaimer:</strong> The information in this webpage
+            is fictitous. <br />
+            It is used for academic purposes only.
+        </footer>
+    </body>
+</html>
